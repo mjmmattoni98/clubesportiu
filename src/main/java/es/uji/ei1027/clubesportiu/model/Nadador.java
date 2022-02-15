1438831ad9591269b745c7b222ae0f3fdacdf4cd
mjmmattoni98 <javier.magni@gmail.com>
2022-02-15 11:32:41 +0100

    proyecto ei1027 club esportiu

---
+++ src/main/java/es/uji/ei1027/clubesportiu/model/Nadador.java
@@ -0,0 +1,70 @@
+package es.uji.ei1027.clubesportiu.model;
+
+import org.springframework.lang.NonNull;
+
+public class Nadador implements Comparable<Nadador> {
+    private String nom;
+    private String numFederat;
+    private String pais;
+    private int edat;
+    private String genere;
+
+    public Nadador() {
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getNumFederat() {
+        return numFederat;
+    }
+
+    public void setNumFederat(String numFederat) {
+        this.numFederat = numFederat;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    public int getEdat() {
+        return edat;
+    }
+
+    public void setEdat(int edat) {
+        this.edat = edat;
+    }
+
+    public String getGenere() {
+        return genere;
+    }
+
+    public void setGenere(String genere) {
+        this.genere = genere;
+    }
+
+    @Override
+    public String toString() {
+        return "Nadador{" +
+                "nom='" + nom + "'" +
+                ", numFederat='" + numFederat + "'" +
+                ", pais='" + pais + "'" +
+                ", edat=" + edat +
+                ", genere='" + genere + "'" +
+                "}";
+    }
+
+    @Override
+    public int compareTo(@NonNull Nadador altre) {
+        return this.getNom().compareTo(altre.getNom());
+    }
+}
